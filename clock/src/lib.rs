9acd62ecb9d41bcfa85da22301d0962f97e20b9e
acassaigne <anthony.cassaigne@gmail.com>
2025-02-21 19:38:59 +0100

    mob next - wip
    lastFile:clock/tests/clock.rs

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,16 +1,27 @@
 #[derive(Debug)]
-pub struct Clock{
+pub struct Clock {
     hours: i32,
     minutes: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Clock{hours, minutes}
+        let total_minutes = hours * 60 + minutes;
+        let minutes_from_midnight = (total_minutes % (24 * 60) + (24 * 60)) % (24 * 60);
+        let total_hours = minutes_from_midnight / 60;
+        let remain_minutes = minutes_from_midnight - total_hours * 60;
+        Clock {
+            hours: total_hours,
+            minutes: remain_minutes,
+        }
     }
 
     pub fn to_string(self) -> String {
-        format!("{hours:02}:{minutes:02}", hours = self.hours, minutes = self.minutes)
+        format!(
+            "{hours:02}:{minutes:02}",
+            hours = self.hours,
+            minutes = self.minutes
+        )
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
